e38ab3d27de51d79b593bda1eadf0c29b7cfba61
Larspiproj <lars.gustavsson@live.com>
2017-10-25 08:39:28 +0100

    showip.c modified

--- showip.c
+++ showip.c
@@ -9,10 +9,10 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-int main(int argc char *argv[]) {
+int main(int argc, char *argv[]) {
 	struct addrinfo hints, *res, *p;
 	int status;
-	char ipstr[INET6_ADDSTRLEN];
+	char ipstr[INET6_ADDRSTRLEN];
 
 	if (argc != 2) {
 		fprintf(stderr, "usage: showip hostname\n");
@@ -29,4 +29,29 @@ int main(int argc char *argv[]) {
 	}
 
 	printf("IP addresses for %s:\n\n", argv[1]);
+
+	for (p = res; p != NULL; p = p->ai_next) {
+		void *addr;
+		char *ipver;
+
+		// get the pointer to the address itself
+		// different fields in IPv4 and IPv6
+		if (p->ai_family == AF_INET) { // IPv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else { //IPv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		// convert the IP to a string and print it:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+
+	freeaddrinfo(res); // free the linked list
+
+	return 0;
 }
